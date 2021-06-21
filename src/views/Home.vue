28e81b73d59bd0cb2ab5a559cb145ae3735c99a0
thomasnguyen704 <thomasnguyen704@gmail.com>
2021-06-21 00:19:59 -0400

    spacing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <article class="py-1 d-flex align-items-center bg-light">
       <b-row class="text-center jumbo-cover">
         <b-col lg="6"></b-col>
-        <b-col lg="6">
-          <p>
+        <b-col lg="6" class="mb-5">
+          <p class="pb-5 mb-5">
             <b-img
               src="https://www.dropbox.com/s/f0wyorwod8ml9ir/cm%20-%20logo%20-%20ff-01b.png?raw=1"
               fluid
@@ -34,7 +34,7 @@ export default {
 
 <style scoped>
 .jumbo-cover {
-  min-height: 80vh;
+  min-height: 85vh;
   min-width: 100vw;
   background-image: url(https://www.dropbox.com/s/u8lddr59lrxze4k/face-bg.png?raw=1);
   background-size: cover;

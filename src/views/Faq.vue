0b4620fafc84e399f0ba464b84f00023ac7a22a7
thomasnguyen704 <thomasnguyen704@gmail.com>
2021-07-10 22:57:43 -0400

    faq

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <h2>Permanent Make-up (Eyebrows, Lips Blushing, Eyeliners)</h2>
+    <b-container class="my-5">
+      <h1 class="lead mt-5">
+        Permanent Make-up (Eyebrows, Lips Blushing, Eyeliners)
+      </h1>
+      <hr />
+      <div v-for="item in faq_pmu" :key="item.title">
+        <p class="strong">{{ item.title }}</p>
+        <p v-html="item.body"></p>
+      </div>
 
-    
-    <h2>Lip Blushing</h2>
+      <h1 class="lead mt-5">Lip Blushing</h1>
+      <hr />
+      <div v-for="item in faq_lip" :key="item.title">
+        <p class="strong">{{ item.title }}</p>
+        <p v-html="item.body"></p>
+      </div>
 
-
-    <h2>Eyeliners</h2>
-    
-    <h2></h2>
+      <h1 class="lead mt-5">Eye Liners</h1>
+      <hr />
+      <div v-for="item in faq_eye" :key="item.title">
+        <p class="strong">{{ item.title }}</p>
+        <p v-html="item.body"></p>
+      </div>
+    </b-container>
   </div>
 </template>
 
@@ -20,23 +35,28 @@ export default {
       faq_pmu: [
         {
           title: "What should you prepare 1-2 weeks before the procedure?",
-          body: "If you are drinking blood thinner medicine, you will need to consult with your doctor whether you can stop drinking blood thinner 5 days before the procedure. If you prone to cold sores/fever blisters, you MUST take an anti-viral Valtrex or Lysine 5-7 days prior to treatment, especially for lips procedure to prevent an outbreak and loss of pigment. Please stop taking antibiotics at least 5 days prior to the procedure. Do not shape wax, tint your brows 3 days before the procedure. • Botox, AHA products, and retinoid should be avoided for 2 weeks prior to the procedure. • Hormone therapies can affect pigmentation and/or cause sensitivity. Contraindications for permanent makeup: The following medical conditions require a note from your doctor giving consent Diabetes Type 1 and 2, high blood pressure, auto-immune disease, thyroid / Graves’ disease. Any other medical condition that causes slow healing, excess bleeding, or a high risk of infection."
+          body:
+            "If you are drinking blood thinner medicine, you will need to consult with your doctor whether you can stop drinking blood thinner 5 days before the procedure. If you prone to cold sores/fever blisters, you MUST take an anti-viral Valtrex or Lysine 5-7 days prior to treatment, especially for lips procedure to prevent an outbreak and loss of pigment. Please stop taking antibiotics at least 5 days prior to the procedure. Do not shape wax, tint your brows 3 days before the procedure. • Botox, AHA products, and retinoid should be avoided for 2 weeks prior to the procedure. • Hormone therapies can affect pigmentation and/or cause sensitivity. Contraindications for permanent makeup: The following medical conditions require a note from your doctor giving consent Diabetes Type 1 and 2, high blood pressure, auto-immune disease, thyroid / Graves’ disease. Any other medical condition that causes slow healing, excess bleeding, or a high risk of infection."
         },
         {
           title: "What should you do 2 days before the procedure?",
-          body: "	You are advised to pick up the numbing cream in order to use and apply a medium-thick layer of numbing cream on the clean and dry designed area 30 minutes (reapply) and 1 hour prior to the procedure and cover the area with clear plastic wrap to activate the numbing. The artist will clean the numbing cream before the procedure. Although numbing cream is used during the procedure, you may still feel sensitivity or discomfort. I cannot accept responsibility if the area to be treated does not respond to the numbing cream. Each individual is different according to skin type. Some clients report the area to be completely numb, while others may experience some discomfort."
+          body:
+            "	You are advised to pick up the numbing cream in order to use and apply a medium-thick layer of numbing cream on the clean and dry designed area 30 minutes (reapply) and 1 hour prior to the procedure and cover the area with clear plastic wrap to activate the numbing. The artist will clean the numbing cream before the procedure. Although numbing cream is used during the procedure, you may still feel sensitivity or discomfort. I cannot accept responsibility if the area to be treated does not respond to the numbing cream. Each individual is different according to skin type. Some clients report the area to be completely numb, while others may experience some discomfort."
         },
         {
           title: "What should you do 24-48 hours before the procedure?",
-          body: "Do NOT consume any alcohol, caffeine, antibiotics, aspirin, niacin, vitamin e, ibuprofen, omega 3, or take any blood thinner medicine. Tylenol is ok to consume. Do NOT work out or sunbathing. Allergy: A patch test may be performed unless waived by the client. If you do suffer from an allergic reaction, you should contact your doctor immediately. Allergic reaction responses may show through redness, swelling, rash, blistering, dryness, or any other symptoms associated with an allergic reaction."
+          body:
+            "Do NOT consume any alcohol, caffeine, antibiotics, aspirin, niacin, vitamin e, ibuprofen, omega 3, or take any blood thinner medicine. Tylenol is ok to consume. Do NOT work out or sunbathing. Allergy: A patch test may be performed unless waived by the client. If you do suffer from an allergic reaction, you should contact your doctor immediately. Allergic reaction responses may show through redness, swelling, rash, blistering, dryness, or any other symptoms associated with an allergic reaction."
         },
         {
           title: "What to expect during the procedure?",
-          body: "A typical PMU appointment lasts from 2-3 hours, and the touch-up lasts from 1-2 hours depending on the work needing to be done. The PMU artist will clean the area, taking a before photo, the designed area is drawn with a pencil, to give the client an idea of shape and form. (Please note that majority of faces are not completely symmetrical. Eyebrows, lips, or eyeliners are not meant to be identical and it is impossible to attain this look even with natural brow hair or natural face shape. It is also not possible to match a photo of your favorite celebrities’ eyebrows, lips, or eyeliners when you may have an entirely different face shape.) Once both the permanent makeup artist and client have agreed on a final shape (ultimately it is the client's decision – but the artist may make recommendations based on their knowledge of what suits a person aesthetically), The artist will shape your eyebrows, lips, or eyeliners then we will decide on the color. Before the shaping and procedure, the numbing anesthetic will be administered to ensure comfort. Once you’re numb, the pigment is deposited. Then, the secondary liquid numb is applied on the open skin before the second round of deposited pigment with the final pigment mask. When the procedure is finished, the artist will take after photos with the client's approval of the finished design, and give the client aftercare instruction."
+          body:
+            "A typical PMU appointment lasts from 2-3 hours, and the touch-up lasts from 1-2 hours depending on the work needing to be done. The PMU artist will clean the area, taking a before photo, the designed area is drawn with a pencil, to give the client an idea of shape and form. (Please note that majority of faces are not completely symmetrical. Eyebrows, lips, or eyeliners are not meant to be identical and it is impossible to attain this look even with natural brow hair or natural face shape. It is also not possible to match a photo of your favorite celebrities’ eyebrows, lips, or eyeliners when you may have an entirely different face shape.) Once both the permanent makeup artist and client have agreed on a final shape (ultimately it is the client's decision – but the artist may make recommendations based on their knowledge of what suits a person aesthetically), The artist will shape your eyebrows, lips, or eyeliners then we will decide on the color. Before the shaping and procedure, the numbing anesthetic will be administered to ensure comfort. Once you’re numb, the pigment is deposited. Then, the secondary liquid numb is applied on the open skin before the second round of deposited pigment with the final pigment mask. When the procedure is finished, the artist will take after photos with the client's approval of the finished design, and give the client aftercare instruction."
         },
         {
           title: "What are the aftercare instructions?",
-          body: "Day 1 (right after the procedure): Gently blot the area with a clean and dry tissue to absorb excess lymph fluid. Do this every 5-10 minutes for about 1- 4 hours until the oozing has stopped. Removing this fluid prevents the hardening of the lymphatic fluids (minimizes scabbing). Keep the area clean and dry, do NOT do anything to make your permanent makeup area sweat, steamy, dusty, or wet 24 hours right after the procedure."
+          body:
+            "Day 1 (right after the procedure): Gently blot the area with a clean and dry tissue to absorb excess lymph fluid. Do this every 5-10 minutes for about 1- 4 hours until the oozing has stopped. Removing this fluid prevents the hardening of the lymphatic fluids (minimizes scabbing). Keep the area clean and dry, do NOT do anything to make your permanent makeup area sweat, steamy, dusty, or wet 24 hours right after the procedure."
         },
         {
           title: "What to avoid?",
@@ -95,20 +115,61 @@ export default {
         },
         {
           title: "What are the post-care instructions?",
-          body: ` <p>Apply the given ointment as often as possible for 1 week. Do not let your lips dry out completely, since this can cause scabbing and cracking of skin surface.</p>
-          <p>For at least 5 days, please do not consume any hot drinks or food. The hotness can result in the pigment fading faster and your lips will peel.</p>
-          <p>For 5 days, please prevent going into a sauna, pool, and taking hot showers/bath. You can get your lips wet (i.e. when washing your face/mouth) but please do not get any hot water near your lip area. In other words, do not soak your face underwater in a bathtub or allow the shower to spray directly in your face.</p>
-          <p>No exercising for 24 hours after the procedure.</p>
-          <p>No lip fillers for 4 weeks post procedure.</p>
-          <p>Do not scrub, scratch, or pick at the tattoo site at any time during the healing process – this can cause you to lose color that may not be repairable.</p>
-          <p>No facials or masks for 15 days, and avoid them in the lips area indefinitely.</p>
-          <p>No Neosporin, Vaseline, or other petroleum products while your lips are healing.</p>
-          <p>	No anti-acne products (i.e. Benzoyl Peroxide, Pro-active) or lightening creams (bleach creams) – this will cause rapid fading.</p>
-          <p>No anti-aging products or facial products containing Alpha Hydroxyls (AHAs), Retin-A, Lactic or Glycolic Acids.</p>`
+          body: ` <ul>
+            <li>Apply the given ointment as often as possible for 1 week. Do not let your lips dry out completely, since this can cause scabbing and cracking of skin surface.</li>
+            <li>For at least 5 days, please do not consume any hot drinks or food. The hotness can result in the pigment fading faster and your lips will peel.</li>
+            <li>For 5 days, please prevent going into a sauna, pool, and taking hot showers/bath. You can get your lips wet (i.e. when washing your face/mouth) but please do not get any hot water near your lip area. In other words, do not soak your face underwater in a bathtub or allow the shower to spray directly in your face.</li>
+            <li>No exercising for 24 hours after the procedure.</li>
+            <li>No lip fillers for 4 weeks post procedure.</li>
+            <li>Do not scrub, scratch, or pick at the tattoo site at any time during the healing process – this can cause you to lose color that may not be repairable.</li>
+            <li>No facials or masks for 15 days, and avoid them in the lips area indefinitely.</li>
+            <li>No Neosporin, Vaseline, or other petroleum products while your lips are healing.</li>
+            <li>No anti-acne products (i.e. Benzoyl Peroxide, Pro-active) or lightening creams (bleach creams) – this will cause rapid fading.</li>
+            <li>No anti-aging products or facial products containing Alpha Hydroxyls (AHAs), Retin-A, Lactic or Glycolic Acids.</li>
+          </ul>`
         },
         {
           title: "Does it hurt?",
           body: `During the procedure, anesthetics will be used to minimize the pain by numbing your lips. Lips are prone to swelling due to their sensitive nature. Swelling will typically subside in 3-5 days`
+        }
+      ],
+      faq_eye: [
+        {
+          title: "What happens during the procedure?",
+          body: `<ul>
+            <li>Step 1 - The design: Your lash line or semi-permanent eyeliner will be designed based on your individual needs, the shape of your eyes, as well as personal style.</li>
+            <li>Step 2 - Color: We will help choose the most natural looking color for you. Our colors are custom blended, non-toxic, they are made from natural mineral-based pigments.</li>
+            <li>Step 3 - Pigmentation: Individual hair follicles are gently layered in the dermal layer of the skin. Cmai Beauty utilizes proprietary technology and a system emulating the look of real eyelashes to create a natural semi-permanent eyeliner. Our Equipment uses a single acupuncture needle that gently vibrates on the dermal layers of the skin depositing a mineral based pigment into the skin. A pigment is precisely applied in thin layers until the ideal color and shape are achieved creating a completely natural look. During the 1st session, we take necessary precautions. Less is more. This session is about getting a nice base started and the fine-tuning takes place in the 2nd appointment.</li>
+            <li>Step 4 - Touch up: 4-6 weeks after your 1st session you’ll return for a touch-up appointment to review the color and shape with your technician and make adjustments if needed to ensure that you have the look that you want to achieve.</li>
+          </ul>`
+        },
+        {
+          title: "Will my eyeliner look normal the next day?",
+          body: `Yes, there will be swelling right after the procedure. The swelling starts coming down right away and in most people, it is back to normal by the following morning.`
+        },
+        {
+          title: "Does the eyeliner procedure hurt?",
+          body: `The eyeliner or lash line procedure is more psychologically scary to most clients but there are very few nerve endings in the lash line so it is an easy procedure. Before starting pigmentation a strong topical numbing cream is applied and reapplied during the procedure. Cmai Beauty uses a single acupuncture needle that gently vibrates on the outer dermal layers of the skin, therefore discomfort is minimal and there is no downtime.`
+        },
+        {
+          title: "Is it safe for me to drive myself after the procedure?",
+          body: `Yes, we use a strong local numbing cream to alleviate discomfort but this does not impair any motor skills and wears off after the procedure concludes.`
+        },
+        {
+          title: "What do I need to know before the eyeliner procedure?",
+          body: `Arrive at your appointment 10 minutes before your scheduled appointment, well rested and relaxed in order to complete paperwork and that you get the most out of your session. Please remove contact lenses before your procedure.`
+        },
+        {
+          title: "Are my eyes open during the eyeliner procedure?",
+          body: `
+          <p>No, your eyes are closed during the entire procedure, although there are frequent short breaks of a minute or two throughout the procedure so you can relax. These breaks also enable the technician to precisely calibrate the application for how it looks with your eyes open. The eyeliner procedure is safe, with a 100% safety record with thousands of satisfied clients treated.</p>
+          <p>Drawing upon techniques from similar surgical and related procedures, experts have refined a technique specifically for Cmai Beauty. The artist will work slowly and carefully with a stable grip on a small section of skin that immobilizes it so cannot move (and you will get many short breaks).</p>
+          <p>New clients are sometimes concerned that the needle could go into the eye. This can never happen due to the equipment, the technique – and the fact that the artist undergoes several months of full-time training and practice before they are certified to treat patients unsupervised. Most clients’ eyes will twitch during the procedure, and this is normal.</p>
+          `
+        },
+        {
+          title: "Can I get the eyelash procedure if I have eyelash extensions?",
+          body: `Yes, although during the process it may cause some of the eyelash extensions to fall `
         }
       ]
     };
@@ -116,4 +177,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.strong {
+  font-weight: 800;
+  margin: 2rem 0 0.5rem 0;
+}
+</style>

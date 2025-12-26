0dd31bc863a4d7d60f225265739ee2a0337f2693
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro-3.local>
2025-12-26 11:29:41 -0500

    added new address and sales

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
   <div>
     <b-modal
       id="my-modal"
       ref="myModal"
-      title="Exciting News! CMai Beauty Studio has Expanded."
+      title="Sale & Promotion"
       header-bg-variant="danger"
       header-text-variant="light"
       hide-footer
     >
-      <p>
-        New Location now mainly open Fridays & Saturdays:
-      </p>
-      <a href="https://maps.app.goo.gl/RQcJRaxeUDecR7f86" target="_blank">
-        <address class="ml-4">
-          C Mai Beauty <br />
-          316 East Blvd #204 <br />
-          Charlotte, NC 28203 <br />
-          (Inside Lewink Lashes & Brows)
-        </address>
-      </a>
-
-      <p>
-        But that’s not all — my Huntersville studio (Exit 25) will remain open
-        through October 9th. On 10/13/2025, we’re moving just a few minutes down
-        the road to a beautiful new space, mainly open Mondays through
-        Thursdays:
-      </p>
-      <a href="https://maps.app.goo.gl/ig5GseUrqHnurKxDA" target="_blank">
-        <address class="ml-4">
-          Carolina Lashes <br />
-          13111 Rosedale Hill Ave <br />
-          Huntersville, NC 28078 <br />
-          (Exit 23 inside Carolina Lashes)
-        </address>
-      </a>
-      <p>
-        Call or text <a href="tel:205-207-2659">(205) 207-2659</a> <br />
-        Book online: www.cmaibeauty.com <br />
-        Follow on Instagram: @cmaibeautystudio <br />
-      </p>
-      <p>
-        Heads up: with the move, pricing will change to align with the new salon
-        rates — details coming soon!
+      <ul>
+        <li>
+          <strong>Full Lash Set Discount</strong> <br />
+          Enjoy 10% off all full lash sets at our Huntersville location. Limited time—book your appointment today!
+        </li>
+        <li>
+          <strong>Client Referral Program</strong> <br />
+          Refer a friend and receive $10 in studio credit for both you and your referral when they book their first service.
+        </li>
+        <li>
+          <strong>Add-On Eyebrow Waxing</strong> <br />
+          Add eyebrow waxing to your lash appointment and enjoy $2 off.
+        </li>
+        <li>
+          <strong>Client Feedback Thank You</strong> <br />
+          Love your experience? Share your genius feedback with a 5-star review on Google, or Facebook mom groups, and receive $2 off per platform as our thank-you.
+        </li>
+      </ul>
+      <p class="mt-4">
+        <em>Reminder: To ensure fairness, only one promotion or discount can be used per appointment.</em>
       </p>
     </b-modal>
     <b-navbar toggleable="lg" type="light" variant="white" sticky ref="navbar">
@@ -55,32 +42,35 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="small">
-            <b-nav-item href="/" @click="scrollMeToNavbar('navbar')"
-              >Home</b-nav-item
-            >
-            <b-nav-item to="services" @click="scrollMeTo('navbar')"
-              >Services</b-nav-item
-            >
-            <b-nav-item to="clients" @click="scrollMeTo('navbar')"
-              >Consultations</b-nav-item
-            >
-            <b-nav-item href="https://carolynn-mai.square.site/"
-              >Products</b-nav-item
-            >
-            <b-nav-item to="terms" @click="scrollMeTo('navbar')"
-              >Terms & Policies</b-nav-item
-            >
-            <b-nav-item to="faq" @click="scrollMeTo('navbar')">FAQ</b-nav-item>
-            <b-nav-item @click="scrollMeTo('footer')"
-              >Contact & Location</b-nav-item
-            >
-            <b-nav-item to="pricing" @click="scrollMeTo('navbar')"
-              >Pricing</b-nav-item
-            >
+            <b-nav-item href="/" @click="scrollMeToNavbar('navbar')">
+              Home
+            </b-nav-item>
+            <b-nav-item to="services" @click="scrollMeTo('navbar')">
+              Services
+            </b-nav-item>
+            <b-nav-item to="clients" @click="scrollMeTo('navbar')">
+              Consultations
+            </b-nav-item>
+            <b-nav-item href="https://carolynn-mai.square.site/">
+              Products
+            </b-nav-item>
+            <b-nav-item to="terms" @click="scrollMeTo('navbar')">
+              Terms & Policies
+            </b-nav-item>
+            <b-nav-item to="faq" @click="scrollMeTo('navbar')">
+              FAQ
+            </b-nav-item>
+            <b-nav-item @click="scrollMeTo('footer')">
+              Contact & Location
+            </b-nav-item>
+            <b-nav-item to="pricing" @click="scrollMeTo('navbar')">
+              Pricing
+            </b-nav-item>
             <b-nav-item
               href="https://squareup.com/appointments/book/kiz99xt2bir5gr/LMQXQF1APV2XD/services"
               target="blank"
-              ><span class="text-danger">Book an Appointment</span>
+            >
+              <span class="text-danger">Book an Appointment</span>
             </b-nav-item>
           </b-navbar-nav>
 
@@ -108,34 +98,45 @@
 
     <footer class="bg-dark text-white" ref="footer">
       <b-container class="py-5">
+
         <b-row>
-          <b-col sm="6" md="2" class="text-center">
-            <b-img src="https://www.dropbox.com/s/2lwotslgjbikr7f/logo.png?raw=1"
-              fluid
-              class="invert"
-            />
-            <p class="my-3">
-              <a href="https://www.facebook.com/carolynnmai8"
-                target="_blank"
-                class="m-1 btn btn-light fa fa-facebook"
-              />
-              <a href="https://www.instagram.com/cmaibeautystudio/"
-                target="_blank"
-                class="m-1 btn btn-light fa fa-instagram"
-              />
+          <b-col>
+            <a href="https://maps.app.goo.gl/hLsPxBg2VgZiDS8W9">
+              <address class="mt-4">
+                C Mai Beauty (Huntersville)<br />
+                13111 Rosedale Hill Ave. <br />
+                Huntersville, NC 28078
+              </address>
+            </a>
+            <p>
+              Find me inside Carolina Lashes. <br />
+              Call or text at <a href="tel:205-207-2659">(205) 207-2659</a>
             </p>
           </b-col>
-          <b-col sm="6" md="4">
+          <b-col>
+            <b-embed
+              frameborder="0"
+              style="border:0"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.817876132296!2d-80.8614253!3d35.409764599999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8856a92e51adca5f%3A0x4afa617b6d23a1a4!2sCarolina%20Lashes%20%26%20Beautique!5e0!3m2!1sen!2sus!4v1766765671255!5m2!1sen!2sus"
+              allowfullscreen
+            />
+          </b-col>
+        </b-row>
+
+        <hr class="my-5 border-grey" />
+
+        <b-row>
+          <b-col>
             <a href="https://maps.app.goo.gl/RQcJRaxeUDecR7f86">
               <address class="mt-4">
-                C Mai Beauty <br />
+                C Mai Beauty (Uptown Chalrotte)<br />
                 316 East Blvd #204<br />
                 Charlotte, NC 28203
               </address>
             </a>
             <p>
-              Find me inside Le Wink Lashes & Brows. Call me at <a href="tel:205-207-2659">(205) 207-2659</a> to schedule a free phone consultation.
-              <!--<a href="tel:205-207-2659">(205) 207-2659</a>-->
+              Find me inside Le Wink Lashes & Brows.<br />
+              Call or text at <a href="tel:205-207-2659">(205) 207-2659</a>
             </p>
           </b-col>
           <b-col>
@@ -145,6 +146,33 @@
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3259.8439504057365!2d-80.85883652339687!3d35.210355772742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88569f7838b0c417%3A0x704b0f582c9b6548!2sLe%20Wink%20Lashes%20%26Brows!5e0!3m2!1sen!2sus!4v1751671432703!5m2!1sen!2sus"
               allowfullscreen
             />
+          </b-col>
+        </b-row>
+
+        <hr class="my-5 border-grey" />
+
+        <b-row class="py-5">
+          <b-col class="text-center">
+            <b-img
+              src="https://www.dropbox.com/s/2lwotslgjbikr7f/logo.png?raw=1"
+              class="invert footer-img"
+            />
+            <p class="my-3">
+              <a
+                href="https://www.facebook.com/carolynnmai8"
+                target="_blank"
+                class="m-1 btn btn-light fa fa-facebook"
+              />
+              <a
+                href="https://www.instagram.com/cmaibeautystudio/"
+                target="_blank"
+                class="m-1 btn btn-light fa fa-instagram"
+              />
+            </p>
+            <p>
+              Call me at <a href="tel:205-207-2659">(205) 207-2659</a>
+              to schedule a free phone consultation.
+            </p>
           </b-col>
         </b-row>
       </b-container>
@@ -183,11 +211,8 @@ export default {
 article {
   padding-bottom: 20vh;
 }
-footer {
-  font-weight: 300;
-}
-footer a {
-  color: skyblue;
+.modal ul li {
+  margin-bottom: 1rem;
 }
 .strong {
   font-weight: 600 !important;
@@ -195,10 +220,19 @@ footer a {
 .invert {
   filter: invert(100%);
 }
+.border-grey {
+  border-color: grey;
+}
+footer {
+  font-weight: 300;
+}
+footer a {
+  color: skyblue;
+}
 footer .fa {
   width: 40px;
 }
-footer img {
-  max-height: 70%;
+.footer-img {
+  width: 25%;
 }
 </style>

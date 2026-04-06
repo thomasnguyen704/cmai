3b9e4aefc43e209f71fc12127eb3d5ec02e08cf8
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro-3.local>
2026-04-05 20:01:57 -0400

    change location

--- src/App.vue
+++ src/App.vue
@@ -49,15 +49,18 @@
             <b-nav-item
               href="https://www.facebook.com/carolynnmai8"
               target="_blank"
+              class="small"
             >
               <span class="fa fa-facebook fa-2x" target="_blank"></span>
             </b-nav-item>
             <b-nav-item
               href="https://www.instagram.com/cmaibeautystudio/"
               target="_blank"
+              class="small"
             >
               <span class="fa fa-instagram fa-2x"></span>
             </b-nav-item>
+
           </b-navbar-nav>
         </b-collapse>
       </b-container>
@@ -69,91 +72,114 @@
 
     <footer class="bg-dark text-white" ref="footer">
       <b-container class="py-5">
-
         <b-row>
-          <b-col>
-            <a href="https://maps.app.goo.gl/hLsPxBg2VgZiDS8W9">
-              <address class="mt-4">
-                C Mai Beauty (Huntersville)<br />
-                13111 Rosedale Hill Ave. <br />
-                Huntersville, NC 28078
-              </address>
-            </a>
-            <p>
-              Find me inside Carolina Lashes. <br />
-              Call or text at <a href="tel:205-207-2659">(205) 207-2659</a>
-            </p>
+          <b-col md="4" class="p-5">
+            <b-row class="py-5">
+              <b-col class="text-center">
+                <b-img
+                  src="https://www.dropbox.com/s/2lwotslgjbikr7f/logo.png?raw=1"
+                  class="invert footer-img"
+                />
+                <p class="my-3">
+                  <a
+                    href="https://www.facebook.com/carolynnmai8"
+                    target="_blank"
+                    class="m-1 btn btn-light fa fa-facebook"
+                  />
+                  <a
+                    href="https://www.instagram.com/cmaibeautystudio/"
+                    target="_blank"
+                    class="m-1 btn btn-light fa fa-instagram"
+                  />
+                </p>
+                <p>
+                  Call me at <a href="tel:205-207-2659">205-207-2659</a>
+                  to schedule a free phone consultation.
+                </p>
+              </b-col>
+            </b-row>
           </b-col>
-          <b-col>
-            <b-embed
-              frameborder="0"
-              style="border:0"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3251.817876132296!2d-80.8614253!3d35.409764599999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8856a92e51adca5f%3A0x4afa617b6d23a1a4!2sCarolina%20Lashes%20%26%20Beautique!5e0!3m2!1sen!2sus!4v1766765671255!5m2!1sen!2sus"
-              allowfullscreen
-            />
-          </b-col>
-        </b-row>
-
-        <hr class="my-5 border-grey" />
-
-        <b-row>
-          <b-col>
-            <a href="https://maps.app.goo.gl/RQcJRaxeUDecR7f86">
-              <address class="mt-4">
-                C Mai Beauty (Uptown Charlotte)<br />
-                316 East Blvd #204<br />
-                Charlotte, NC 28203
-              </address>
-            </a>
-            <p>
-              Find me inside Le Wink Lashes & Brows.<br />
-              Call or text at <a href="tel:205-207-2659">205-207-2659</a>
-            </p>
-          </b-col>
-          <b-col>
-            <b-embed
-              frameborder="0"
-              style="border:0"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3259.8439504057365!2d-80.85883652339687!3d35.210355772742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88569f7838b0c417%3A0x704b0f582c9b6548!2sLe%20Wink%20Lashes%20%26Brows!5e0!3m2!1sen!2sus!4v1751671432703!5m2!1sen!2sus"
-              allowfullscreen
-            />
-          </b-col>
-        </b-row>
-
-        <hr class="my-5 border-grey" />
-
-        <b-row class="py-5">
-          <b-col class="text-center">
-            <b-img
-              src="https://www.dropbox.com/s/2lwotslgjbikr7f/logo.png?raw=1"
-              class="invert footer-img"
-            />
-            <p class="my-3">
-              <a
-                href="https://www.facebook.com/carolynnmai8"
-                target="_blank"
-                class="m-1 btn btn-light fa fa-facebook"
-              />
-              <a
-                href="https://www.instagram.com/cmaibeautystudio/"
-                target="_blank"
-                class="m-1 btn btn-light fa fa-instagram"
-              />
-            </p>
-            <p>
-              Call me at <a href="tel:205-207-2659">205-207-2659</a>
-              to schedule a free phone consultation.
-            </p>
+          <b-col md="8" class="p-5">
+            <b-row>
+              <b-col>
+                <a href="https://maps.app.goo.gl/zxv5KS4gwEcbLabx5"> 
+                  <address class="mt-4">
+                    C Mai Beauty (Davidson)<br />
+                    452 S. Main St. Suite 105<br />
+                    Davidson, NC 28078
+                  </address>
+                </a>
+                <p>
+                  Find me inside Beautiful You by Christine. <br />
+                  Call or text at <a href="tel:205-207-2659">(205) 207-2659</a>
+                </p>
+              </b-col>
+              <b-col>
+                <b-embed
+                  frameborder="0"
+                  style="border:0"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3248.3794641173454!2d-80.85623312380511!3d35.4948941726479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8856aa3325650a75%3A0x3777a2a16b917b32!2s452%20S%20Main%20St%2C%20Davidson%2C%20NC%2028036!5e0!3m2!1sen!2sus!4v1775431188572!5m2!1sen!2sus"
+                  allowfullscreen
+                  aspect="16by9"
+                />
+              </b-col>
+            </b-row>
+            <hr class="my-5 border-grey" />
+            <b-row>
+              <b-col>
+                <a href="https://maps.app.goo.gl/RQcJRaxeUDecR7f86">
+                  <address class="mt-4">
+                    C Mai Beauty (Uptown Charlotte)<br />
+                    316 East Blvd Suite 204<br />
+                    Charlotte, NC 28203
+                  </address>
+                </a>
+                <p>
+                  Find me inside Le Wink Lashes & Brows.<br />
+                  Call or text at <a href="tel:205-207-2659">205-207-2659</a>
+                </p>
+              </b-col>
+              <b-col>
+                <b-embed
+                  frameborder="0"
+                  style="border:0"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3259.8439504057365!2d-80.85883652339687!3d35.210355772742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88569f7838b0c417%3A0x704b0f582c9b6548!2sLe%20Wink%20Lashes%20%26Brows!5e0!3m2!1sen!2sus!4v1751671432703!5m2!1sen!2sus"
+                  allowfullscreen
+                  aspect="16by9"
+                />
+              </b-col>
+            </b-row>
           </b-col>
         </b-row>
       </b-container>
     </footer>
+
+  <b-modal ref="myModal" title="Hello Gorgeous" hide-footer>
+    <p> A new chapter begins. Cmai Beauty Studio has officially moved from Huntersville to a private luxury suite in Davidson.</p>
+    <p>Designed with intention, this space was created to offer a more elevated, intimate, and personalized experience - because you deserve nothing less than exceptional.</p>
+
+    <p><b>Grand Opening: April 13, 2026</b></p>
+    
+    <a href="https://maps.app.goo.gl/RQcJRaxeUDecR7f86">
+      <address class="mt-4">
+        C Mai Beauty (Davidson)<br />
+        452 S. Main St. Suite 105<br />
+        Davidson, NC 28078
+      </address> 
+    </a>
+
+    <p>Inside Beautiful You by Christine | Same building as State Farm. I can't wait to welcome you into this refined new space</p>
+  </b-modal>
+
   </div>
 </template>
 
+
 <script>
 export default {
-  //mounted() {this.$refs.myModal.show();},
+  mounted() {
+    this.$refs.myModal.show();
+  },
   methods: {
     scrollMeTo(refName) {
       var element = this.$refs[refName];
@@ -202,6 +228,6 @@ footer .fa {
   width: 40px;
 }
 .footer-img {
-  width: 25%;
+  width: 90%;
 }
 </style>
